e5f63ce6e49b04ed16fb7a556e97a7e6ac98b76d
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-11 22:39:16 +0100

    unit test is now green

--- tdd-python/from_videos/superlists/lists/tests.py
+++ tdd-python/from_videos/superlists/lists/tests.py
@@ -9,7 +9,7 @@ class HomePageTet(TestCase):
 
     def test_home_page_is_about_todo_lists(self):
         request = HttpRequest()
-        reponse = home_page(request)
-        self.assertTrue(response.content.startswith('<html>'))
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do Lists </title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
