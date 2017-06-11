7f7e640b2cf03e57e45a05b6e4e9432803e2cb3d
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-11 22:33:52 +0100

    first unit test for home page

--- tdd-python/from_videos/superlists/lists/tests.py
+++ tdd-python/from_videos/superlists/lists/tests.py
@@ -1,8 +1,15 @@
+from django.http import HttpRequest
 from django.test import TestCase
+
+from lists.views import home_page
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
+class HomePageTet(TestCase):
 
-    def test_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_home_page_is_about_todo_lists(self):
+        request = HttpRequest()
+        reponse = home_page(request)
+        self.assertTrue(response.content.startswith('<html>'))
+        self.assertIn(b'<title>To-Do Lists </title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
