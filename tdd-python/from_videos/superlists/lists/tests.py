2f5918a02f7c1a4d6ccb3db01cf6d79d6aebeb76
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-13 16:08:30 +0100

    test on template - old way of testing home page removed

--- tdd-python/from_videos/superlists/lists/tests.py
+++ tdd-python/from_videos/superlists/lists/tests.py
@@ -10,11 +10,6 @@ class HomePageTet(TestCase):
     def test_home_page_is_about_todo_lists(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do Lists</title>', response.content)
-        self.assertTrue(response.content.strip().endswith(b'</html>'))
-
         with open('lists/templates/home.html') as f:
             expected_content = f.read()
-
         self.assertEqual(response.content.decode(), expected_content)
