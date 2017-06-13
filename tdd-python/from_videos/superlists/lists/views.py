bc195d62a19676cd2d5aec24620b6b11378d2d57
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-13 07:12:14 +0100

    aesthetic change

--- tdd-python/from_videos/superlists/lists/views.py
+++ tdd-python/from_videos/superlists/lists/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do Lists </title></html>')
+    return HttpResponse('<html><title>To-Do Lists</title></html>')
