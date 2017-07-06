9b74e7434cd0607c4137902ef20751cc679f170f
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-06 07:44:12 +0100

    default value for attribute 'text', Item model

--- tdd-python/from_videos/superlists/lists/models.py
+++ tdd-python/from_videos/superlists/lists/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     # I want attribute 'text' to be a text field in the database, and be taken into account for persistence
-    text = models.TextField()
+    text = models.TextField(default='')
