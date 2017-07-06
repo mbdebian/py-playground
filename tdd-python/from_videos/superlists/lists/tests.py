f131a826708bcbc2da66e817dc152aa106c622c3
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-06 07:24:56 +0100

    PEP 8 compliant and new test case placeholder

--- tdd-python/from_videos/superlists/lists/tests.py
+++ tdd-python/from_videos/superlists/lists/tests.py
@@ -5,6 +5,7 @@ import re
 
 from lists.views import home_page
 
+
 # Helpers
 def remove_csrf(html_code):
     """ In this version of Django we can't predict the csrf_token, so we need to
@@ -12,10 +13,10 @@ def remove_csrf(html_code):
     csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
     return re.sub(csrf_regex, '', html_code)
 
+
 # Create your tests here.
 
 class HomePageTet(TestCase):
-
     def test_home_page_is_about_todo_lists(self):
         request = HttpRequest()
         response = home_page(request)
@@ -25,11 +26,15 @@ class HomePageTet(TestCase):
         print("=" * 20 + " RESPONSE " + "=" * 20)
         print(remove_csrf(response.content.decode()))
         self.assertEqual(remove_csrf(response.content.decode()), remove_csrf(expected_content))
-        #print("This test fails due to a change in Django API that I still have to figure out")
+        # print("This test fails due to a change in Django API that I still have to figure out")
 
-    #def test_home_page_can_remember_post_request(self):
-        #request = HttpRequest()
-        #request.method = 'POST'
-        #request.POST['item_text'] = 'A new item'
-        #response = home_page(request)
-        #self.assertIn('A new item', response.content.decode())
+        # def test_home_page_can_remember_post_request(self):
+        # request = HttpRequest()
+        # request.method = 'POST'
+        # request.POST['item_text'] = 'A new item'
+        # response = home_page(request)
+        # self.assertIn('A new item', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+    pass
