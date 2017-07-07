109b57f2bc2621ddffa687e9229273180b77f26c
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-07 06:01:11 +0100

    now the view stores the data in the database, GREEN

--- tdd-python/from_videos/superlists/lists/views.py
+++ tdd-python/from_videos/superlists/lists/views.py
@@ -7,6 +7,7 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
         return render(request, 'home.html', {'new_item_text': new_item_text})
     else:
         return render(request, 'home.html', {'new_item_text': ''})
