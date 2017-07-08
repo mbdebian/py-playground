c457cfbf65ca5738d98dd9f4ead6fd1b36dc44b3
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-08 07:37:31 +0100

    make home page view return a redirect on POST request

--- tdd-python/from_videos/superlists/lists/views.py
+++ tdd-python/from_videos/superlists/lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -8,7 +8,7 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-        return render(request, 'home.html', {'new_item_text': new_item_text})
+        return redirect('/')
     else:
         items = Item.objects.all()
         return render(request, 'home.html', {'new_item_text': '', 'items': items})
