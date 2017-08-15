fb56b901edefc08004588e9c15e0b2984c4298da
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-08-15 10:25:54 +0100

    header and module docs, now for real

---
+++ dirty_code/fisheye_tools/process_reports.py
@@ -0,0 +1,13 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : fisheye_tools
+# Timestamp : 15-08-2017 10:24
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Sample code for processing fisheye reports
+"""
+
