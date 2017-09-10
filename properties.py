22d64e75031af43d57f90843a0026b86d92818e8
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-10 06:32:04 +0100

    playing with properties, placeholder

---
+++ properties.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : py-playground
+# Timestamp : 10-09-2017 6:31
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
