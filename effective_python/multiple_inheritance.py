26b9a5897676a22d1a23bb06d3b8e97bddb7ad1a
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-30 07:39:06 +0100

    multiple inheritance example, first subclass

--- effective_python/multiple_inheritance.py
+++ effective_python/multiple_inheritance.py
@@ -4,4 +4,9 @@ class MyBaseClass(object):
     def __init__(self, value):
         self.value = value
 
+class TimesFive(MyBaseClass):
+    def __init__(self, value):
+        super().__init__(value)
+        self.value *= 5
+
 
