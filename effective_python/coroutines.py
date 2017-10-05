ec9c30d237db00859a78ada31e778b70e89e62ae
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-05 08:45:31 +0100

    usage example for the coroutine

--- effective_python/coroutines.py
+++ effective_python/coroutines.py
@@ -16,3 +16,8 @@ def my_coroutine():
     while True:
         received = yield
         print('Received: ', received)
+
+it = my_coroutine()
+next(it)            # Prime the coroutine
+it.send('First')
+it.send('Second')
