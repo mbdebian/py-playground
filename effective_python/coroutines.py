84c9b7505c1f7f29dabd66701dc3bd00903c0cae
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-05 08:42:29 +0100

    coroutines example, placeholder

---
+++ effective_python/coroutines.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : py-playground
+# Timestamp : 05-10-2017 8:41
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
