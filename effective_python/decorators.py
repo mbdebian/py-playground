2bb328ae7ac87a48c0f483fa6670a7f5902ca458
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-06 06:39:17 +0100

    use @wraps decorator

--- effective_python/decorators.py
+++ effective_python/decorators.py
@@ -11,8 +11,11 @@
 Playground for decorators in Python
 """
 
+from functools import wraps
+
 
 def trace(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         print("{}({},{}) -> {}".format(func.__name__, args, kwargs, result))
