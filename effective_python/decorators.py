7e4d8c8b712bd291d4664db84e9a5024bacbe1ba
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-06 06:28:33 +0100

    wrapper for tracing functions

--- effective_python/decorators.py
+++ effective_python/decorators.py
@@ -10,3 +10,12 @@
 """
 Playground for decorators in Python
 """
+
+
+def trace(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print("{}({},{}) -> {}".format(func.__name__, args, kwargs, result))
+        return result
+    return wrapper
+
