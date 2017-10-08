773a6b123f03161bd134581878603b075b6a6f8d
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-08 06:50:28 +0100

    playing with contextlib

---
+++ effective_python/contexts.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : py-playground
+# Timestamp : 08-10-2017 6:50
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
