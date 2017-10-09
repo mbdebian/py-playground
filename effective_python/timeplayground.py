0617d8a5e427bd76cc312f3242a4eb95a291ae8a
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-09 14:54:07 +0100

    now, the other way around

--- effective_python/timeplayground.py
+++ effective_python/timeplayground.py
@@ -11,10 +11,13 @@
 Playing with time and datetime
 """
 
-from time import localtime, strftime
+from time import localtime, strftime, mktime, strptime
 
+# From time to local time
 now = 1407694710
 local_tuple = localtime(now)
 time_format = '%Y-%m-%d %H:%M:%S'
 time_str = strftime(time_format, local_tuple)
 print(time_str)
+
+# From local time to UTC
