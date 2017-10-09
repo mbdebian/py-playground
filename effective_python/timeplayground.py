2c06eb3bc8291e2e3dae25af163986414a0c2b6d
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-09 15:23:20 +0100

    datetime approach

--- effective_python/timeplayground.py
+++ effective_python/timeplayground.py
@@ -27,3 +27,7 @@ utc_now = mktime(time_tuple)
 print(utc_now)
 
 # Now using datetime module
+now = datetime(2014, 8, 10, 18, 18, 30)
+now_utc = now.replace(tzinfo=timezone.utc)
+now_local = now_utc.astimezone()
+print(now_local)
