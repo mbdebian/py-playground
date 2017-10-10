02d8c6c1dde9bb161a000eb27694344b3fbe74ad
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-10 13:27:32 +0100

    placeholder playground for memory leaks

---
+++ effective_python/memory_leaks.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : py-playground
+# Timestamp : 10-10-2017 13:27
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
