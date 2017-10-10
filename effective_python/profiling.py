487c36bd16f7e6a7a41e3619072c0cc50e250d73
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-10 12:42:56 +0100

    profiling, placeholder

---
+++ effective_python/profiling.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : py-playground
+# Timestamp : 10-10-2017 12:42
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
