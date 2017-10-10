42cc7246d8d8b9528978d13528b257635ac7c491
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-10-10 12:51:25 +0100

    use the insertion sort algorithm

--- effective_python/profiling.py
+++ effective_python/profiling.py
@@ -11,6 +11,8 @@
 This module is for playing with profiling applications in python
 """
 
+from random import randint
+
 
 def insertion_sort(data):
     result = []
@@ -25,3 +27,8 @@ def insert_value(array, value):
             array.insert(i, value)
             return
     array.append(value)
+
+
+max_size = 10 ** 4
+data = [randint(0, max_size) for _ in range(max_size)]
+test = lambda: insertion_sort(data)
